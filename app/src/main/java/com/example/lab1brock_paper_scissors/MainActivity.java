cb2962f59c48e697faceaef723475909602378d5
Matthew Hayes <matthewhayes002@gmail.com>
2024-01-30 00:56:40 -0600

    Added Button names & view binding

--- app/src/main/java/com/example/lab1brock_paper_scissors/MainActivity.java
+++ app/src/main/java/com/example/lab1brock_paper_scissors/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.lab1brock_paper_scissors;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.lab1brock_paper_scissors.databinding.ActivityMainBinding;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        int playerScore, comScore;
+        Random rand = new Random();
+
+        TextView rt = binding.rockButton;
+        rt.setText(Weapon.ROCK.toString());
+
+        TextView pt = binding.paperButton;
+        pt.setText(Weapon.PAPER.toString());
+
+        TextView st = binding.scissorsButton;
+        st.setText(Weapon.SCISSORS.toString());
+
+        binding.rockButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
