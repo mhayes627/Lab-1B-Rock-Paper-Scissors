16c19340de683bbce00731be3ab285ed69ebdf11
Matthew Hayes <mhayes6@stu.jsu.edu>
2024-01-30 14:13:44 -0600

    Started game logic

--- app/src/main/java/com/example/lab1brock_paper_scissors/MainActivity.java
+++ app/src/main/java/com/example/lab1brock_paper_scissors/MainActivity.java
@@ -9,10 +9,17 @@ import android.widget.TextView;
 
 import com.example.lab1brock_paper_scissors.databinding.ActivityMainBinding;
 
+import org.w3c.dom.Text;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    int playerScore = 0;
+    int comScore = 0;
+    String comChoice;
+    String playerChoice;
+    Random rand = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +27,6 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-
-        int playerScore, comScore;
-        Random rand = new Random();
 
         TextView rt = binding.rockButton;
         rt.setText(Weapon.ROCK.toString());
@@ -33,13 +37,46 @@ public class MainActivity extends AppCompatActivity {
         TextView st = binding.scissorsButton;
         st.setText(Weapon.SCISSORS.toString());
 
+        TextView score = binding.scoreText;
+        score.setText(getResources().getString(R.string.score_text, playerScore, comScore));
+
         binding.rockButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                playerChoice = binding.rockButton.getText().toString();
+                startMatch(playerChoice);
+            }
+        });
+        binding.paperButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playerChoice = binding.paperButton.getText().toString();
+                startMatch(playerChoice);
+            }
+        });
+        binding.scissorsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playerChoice = binding.scissorsButton.getText().toString();
+                startMatch(playerChoice);
 
             }
         });
 
 
     }
+
+    public void startMatch(String pc){
+        binding.pweaponText.setText(pc);
+        int weapon = rand.nextInt(Weapon.values().length);
+        comChoice = Weapon.values()[weapon].toString();
+
+        TextView cWeaponText = binding.cweaponText;
+        cWeaponText.setText(comChoice);
+
+        TextView wt = binding.winText;
+
+
+    }
+
 }
